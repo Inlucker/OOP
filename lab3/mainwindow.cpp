79ae13281dbd0fb40ee2a4b24274b6b701af928a
Arseny Pronin <arseny707@mail.ru>
2021-06-02 04:15:05 +0300

    added move and scale by RMB and mouseWheel

--- lab3/mainwindow.cpp
+++ lab3/mainwindow.cpp
@@ -42,39 +42,101 @@ void MainWindow::mouseReleaseEvent(QMouseEvent *event)
     {
         LMB_is_pressed = false;
     }
+
+    if (event->button() == Qt::RightButton)
+    {
+        RMB_is_pressed = true;
+    }
 }
 
 void MainWindow::mousePressEvent(QMouseEvent *event)
 {
+    previous_x = event->position().x();
+    previous_y = event->position().y();
     if (event->button() == Qt::LeftButton && !LMB_is_pressed && this->canvas->rect().contains(event->pos()))
     {
         //cout << "Set" << endl;
-        previous_x = event->position().x();
-        previous_y = event->position().y();
+        //previous_x = event->position().x();
+        //previous_y = event->position().y();
         LMB_is_pressed = true;
+    }
+    if (event->button() == Qt::RightButton && !RMB_is_pressed && this->canvas->rect().contains(event->pos()))
+    {
+        //cout << "Set" << endl;
+        //previous_x = event->position().x();
+        //previous_y = event->position().y();
+        RMB_is_pressed = true;
     }
 }
 
 #define ROTATE_SPEED 5
+#define MOVE_SPEED 1
 
 void MainWindow::mouseMoveEvent(QMouseEvent *event)
 {
     if (LMB_is_pressed)
     {
         double x = double(previous_x - event->position().x()) / ROTATE_SPEED;
-        double y = double(-previous_y + event->position().y()) / ROTATE_SPEED;
+        double y = double(previous_y - event->position().y()) / ROTATE_SPEED;
 
 
-        TransformModel transformCmd(0, Point(0, 0, 0), Point(1, 1, 1), Point(-y, x, 0));
+        TransformModel transformCmd(0, Point(0, 0, 0), Point(1, 1, 1), Point(y, x, 0));
+        interface->execute(transformCmd);
+
+        DrawScene drawCmd;
+        interface->execute(drawCmd);
+
+        /*canvas->update();
+
+        previous_x = event->position().x();
+        previous_y = event->position().y();*/
+    }
+    else if (RMB_is_pressed)
+    {
+        double x = double(previous_x - event->position().x()) / MOVE_SPEED;
+        double y = double(previous_y - event->position().y()) / MOVE_SPEED;
+
+
+        TransformModel transformCmd(0, Point(-x, -y, 0), Point(1, 1, 1), Point(0, 0, 0));
+        interface->execute(transformCmd);
+
+        DrawScene drawCmd;
+        interface->execute(drawCmd);
+
+        /*canvas->update();
+
+        previous_x = event->position().x();
+        previous_y = event->position().y();*/
+    }
+
+    canvas->update();
+
+    previous_x = event->position().x();
+    previous_y = event->position().y();
+}
+
+
+#define SCALE_SPEED 10
+
+void MainWindow::wheelEvent(QWheelEvent *event)
+{
+    if (this->canvas->rect().contains(int(event->position().x()), int(event->position().y())))
+    {
+        QPoint numDegrees = event->angleDelta() / 120;
+        cout <<  numDegrees.x() << endl;
+        cout <<  numDegrees.y() << endl;
+        double kx = 1 + double(numDegrees.x()) / SCALE_SPEED;
+        double ky = 1 + double(numDegrees.y()) / SCALE_SPEED;
+        cout <<  kx << endl;
+        cout <<  ky << endl;
+
+        TransformModel transformCmd(0, Point(0, 0, 0), Point(ky, ky, ky), Point(0, 0, 0));
         interface->execute(transformCmd);
 
         DrawScene drawCmd;
         interface->execute(drawCmd);
 
         canvas->update();
-
-        previous_x = event->position().x();
-        previous_y = event->position().y();
     }
 }
 
--- lab3/mainwindow.h
+++ lab3/mainwindow.h
@@ -25,6 +25,7 @@ protected:
    virtual void mouseReleaseEvent(QMouseEvent *event);
    virtual void mousePressEvent(QMouseEvent *event);
    virtual void mouseMoveEvent(QMouseEvent *event);
+   virtual void wheelEvent(QWheelEvent *event);
 
 private slots:
     void on_SetSceneBtn_clicked();
@@ -46,6 +47,7 @@ private:
     shared_ptr<Facade> interface;
 
     bool LMB_is_pressed = false;
+    bool RMB_is_pressed = false;
     int previous_x = 0, previous_y = 0;
     //shared_ptr<QPainter> painter;
     Ui::MainWindow *ui;
